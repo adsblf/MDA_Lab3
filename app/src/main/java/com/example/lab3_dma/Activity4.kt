3e75225b2ff04a5fb1ff2dc44ec3d27a2ad121b4
Aleksey Berezhnov <v4tary@yandex.ru>
2023-04-07 06:32:48 +0600

    Add new fragments

---
+++ app/src/main/java/com/example/lab3_dma/Activity4.kt
@@ -0,0 +1,32 @@
+package com.example.lab3_dma
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.FrameLayout
+import android.widget.ImageButton
+import android.widget.ImageView
+import kotlin.random.Random
+
+class Activity4 : AppCompatActivity() {
+    val imageArray: IntArray = intArrayOf(
+        R.drawable.image_1,
+        R.drawable.image_2,
+        R.drawable.image_3,
+        R.drawable.image_4,
+        R.drawable.image_5,
+    )
+    val random: Random = Random
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_4)
+        val layout: FrameLayout = findViewById(R.id.frameLayout)
+        val imageView: ImageView = findViewById(R.id.imageView)
+        val imageButton: ImageButton = findViewById(R.id.imageButton)
+        imageView.setImageResource(imageArray[0])
+        imageButton.setOnClickListener {
+            val randomNumber = random.nextInt(imageArray.size)
+            imageView.setImageResource(imageArray[randomNumber])
+        }
+    }
+
+}
